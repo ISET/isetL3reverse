c02ed44772ad463df18f839be5915f265a68a981
Brian Wandell <wandell@stanford.edu>
2015-12-02 23:55:15 -0800

    Flower image analysis added.

--- nikon/l3NikonError.m
+++ nikon/l3NikonError.m
@@ -62,3 +62,43 @@ vcNewGraphWin; imshow(rgb3)   % Rendered and Corrected by T
 imwrite(rgb3,'DSC_0805CC.JPG','jpg');
 
 %% Now check a pretty good one, say one with a red flower
+
+rgb1 = imread('original/DSC_0784.JPG');
+rgb2 = imread('rendered/DSC_0784.JPG');
+
+[rgb1,r,c] = RGB2XWFormat(rgb1);
+[rgb2] = RGB2XWFormat(rgb2);
+
+% rgb1 = rgb2 * T
+T = double(rgb2) \ double(rgb1);
+
+rgb3 = double(rgb2) * T;
+rgb3 = uint8(ieClip(rgb3,0,255));
+
+rgb3 = XW2RGBFormat(rgb3,r,c);
+rgb2 = XW2RGBFormat(rgb2,r,c);
+rgb1 = XW2RGBFormat(rgb1,r,c);
+
+%%
+vcNewGraphWin; imshow(rgb1); title('Original'); % Original
+vcNewGraphWin; imshow(rgb3); title('Corrected');   % Rendered and Corrected by T
+
+%% Now compare the SCIELAB error after transform
+
+xyz1 = srgb2xyz(double(rgb1)/255);
+xyz3 = srgb2xyz(double(rgb3)/255);
+
+%%
+d = displayCreate;
+d = displaySet(d,'viewing distance',1);
+p = scParams(displayGet(d,'dpi'),displayGet(d,'viewing distance'));
+wPoint = displayGet(d,'white point');
+wPoint = 2*wPoint/wPoint(2);  % Luminance of 1 for white point, as per srgb2xyz
+dE = scielab(xyz1,xyz3,wPoint,p);
+
+%%
+mean(dE(:))
+hist(dE(:),100)
+vcNewGraphWin; imagesc(dE); colorbar; colormap(gray); axis image; axis off
+
+
