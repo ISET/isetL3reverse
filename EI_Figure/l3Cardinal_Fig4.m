0212f0d9da0f936e4044386d7d5805063cc0cdab
Haomiao Jiang <Killua@HJ-Air.local>
2016-01-16 21:09:54 -0800

    routines for figures for EI paper

--- EI_Figure/l3Cardinal_Fig4.m
+++ EI_Figure/l3Cardinal_Fig4.m
@@ -7,7 +7,7 @@
 % Init ISET session
 ieInit;
 
-% Init remote data toolbox parameters 
+% Init remote data toolbox parameters
 rd = RdtClient('scien');
 rd.crp('/L3/Cardinal/D600');
 
@@ -19,7 +19,8 @@ pad_sz   = (patch_sz - 1) / 2;
 %% Load a pair of raw and rendered file
 % search for remote path of rendered file
 % fileName = 'Pl_KaladanRiver_Rakhine_0818'; vertical image
-fileName = 'ma_griz_39558';  % training file name
+% fileName = 'edl_lakeinle_0850';  % training file name
+fileName = 'ma_griz_39558';
 rdTif = rd.searchArtifacts([fileName '_dxo_nodist'], 'type', 'tif');
 
 % load image
@@ -34,7 +35,7 @@ sz = [size(tif, 1) size(tif, 2)];
 
 % Offset for Cardinal, D600
 if sz(1) > sz(2) % vertical
-    offset = [24 1]; 
+    offset = [24 1];
 else % horizontal
     offset = [1 -23];
 end
@@ -66,18 +67,12 @@ l3t.l3c.cutPoints = {logspace(-3.8, -1.5, 60), []};
 % learn linear filters
 l3t.train(l3d);
 
-% visualize kernels
-% l3t.plot('kernel mean',2)
-
 %% Render the training image
-I_rawTest = I_raw;  % For first Cardinal case
-tifTest = tif;
-
 % Render
 l3r = l3Render();
-l3_RGB = ieClip(l3r.render(I_rawTest, cfa, l3t), 0, 1);
-vcNewGraphWin([], 'wide'); 
-subplot(1, 2, 1); imshow(tifTest); title('Nikon D600');
+l3_RGB = ieClip(l3r.render(I_raw, cfa, l3t), 0, 1);
+vcNewGraphWin([], 'wide');
+subplot(1, 2, 1); imshow(tif); title('Nikon D600');
 subplot(1, 2, 2); imshow(l3_RGB); title('L3 Rendered');
 
 %% Comptue S-CIELAB difference
@@ -98,3 +93,115 @@ vcNewGraphWin; imagesc(de, [0 7]); axis image; axis off; colorbar;
 vcNewGraphWin; hist(de(:), 100);
 fprintf('Mean S-CIELab DeltaE is: %.3f\n', mean(de(:)));
 fprintf('Std of S-CIELab DeltaE is: %.3f\n', std(de(:)));
+
+%% Testing on another image
+%  Load testing file
+fileName = 'ma_mountaingorillas_0510';  % test file name
+rdTif = rd.searchArtifacts([fileName '_dxo_nodist'], 'type', 'tif');
+
+% load image
+localFile = [tempname '.tif'];
+rdtSave(localFile, rdTif(1).url);
+
+tif = im2double(imread(localFile));
+if isodd(size(tif, 1)), tif = tif(1:end-1, :, :); end
+if isodd(size(tif, 2)), tif = tif(:, 1:end-1, :); end
+sz = [size(tif, 1) size(tif, 2)];
+
+% Offset for Cardinal, D600
+if sz(1) > sz(2) % vertical
+    offset = [24 1];
+else % horizontal
+    offset = [1 -23];
+end
+
+% clean up
+delete(localFile);
+
+% search for remote path of raw file
+rdRaw = rd.searchArtifacts(fileName, 'type', 'pgm');
+
+% load raw data
+localFile = [tempname '.pgm'];
+rdtSave(localFile, rdRaw(1).url);
+
+I_raw = im2double(imread(localFile));
+I_raw = rawAdjustSize(I_raw, sz, pad_sz, offset);
+
+% clean up
+delete(localFile);
+
+% Render on file
+l3r = l3Render();
+l3_RGB = l3r.render(I_raw(2:end-1, 2:end-1), cfa, l3t);
+
+% illuminant correction
+tif = tif(2:end-1, 2:end-1, :);
+[l3_XW, r, c] = RGB2XWFormat(l3_RGB);
+m = pinv(l3_XW) * RGB2XWFormat(tif);
+l3_RGB_corrected = ieClip(XW2RGBFormat(l3_XW * m, r, c), 0 , 1);
+
+vcNewGraphWin([], 'wide');
+subplot(1, 2, 1); imshow(tif); title('Nikon D600');
+subplot(1, 2, 2); imshow(l3_RGB_corrected); title('L3 Rendered');
+
+%% Comptue S-CIELAB difference
+% Init parameters
+d = displayCreate('LCD-Apple');
+d = displaySet(d, 'gamma', 'linear');  % use a linear gamma table
+d = displaySet(d, 'viewing distance', 1);
+rgb2xyz = displayGet(d, 'rgb2xyz');
+wp = displayGet(d, 'white xyz'); % white point
+params = scParams;
+params.sampPerDeg = displayGet(d, 'dots per deg');
+
+% Compute difference
+xyz1 = imageLinearTransform(l3_RGB_corrected, rgb2xyz);
+xyz2 = imageLinearTransform(tif, rgb2xyz);
+de = scielab(xyz1, xyz2, wp, params);
+vcNewGraphWin; imagesc(de, [0 7]); axis image; axis off; colorbar;
+vcNewGraphWin; hist(de(:), 100);
+fprintf('Mean S-CIELab DeltaE is: %.3f\n', mean(de(:)));
+fprintf('Std of S-CIELab DeltaE is: %.3f\n', std(de(:)));
+
+%% Test on more images
+s = rd.searchArtifacts('_dxo_nodist');
+de = zeros(length(s), 1);
+offset = [1 -23]; l3r = l3Render();
+
+for ii = 1 : length(s)
+    
+    % load image
+    localFile = [tempname '.tif'];
+    rdtSave(localFile, s(ii).url);
+    
+    tif = im2double(imread(localFile));
+    if size(tif, 1) > size(tif, 2), continue; end
+    if isodd(size(tif, 1)), tif = tif(1:end-1, :, :); end
+    if isodd(size(tif, 2)), tif = tif(:, 1:end-1, :); end
+    delete(localFile);
+    
+    % search for remote path of raw file
+    fileName = s(ii).artifactId(1:end-11);
+    rdRaw = rd.searchArtifacts(fileName, 'type', 'pgm');
+    
+    % load raw data
+    localFile = [tempname '.pgm'];
+    rdtSave(localFile, rdRaw(1).url);
+    
+    I_raw = im2double(imread(localFile));
+    sz = [size(tif, 1) size(tif, 2)];
+    I_raw = rawAdjustSize(I_raw, sz, pad_sz, offset);
+    delete(localFile);
+    
+    % Render on file
+    l3_RGB = l3r.render(I_raw, cfa, l3t);
+    [l3_XW, r, c] = RGB2XWFormat(l3_RGB);
+    m = pinv(l3_XW) * RGB2XWFormat(tif);
+    l3_RGB_corrected = ieClip(XW2RGBFormat(l3_XW * m, r, c), 0 , 1);
+    
+    xyz1 = imageLinearTransform(l3_RGB_corrected, rgb2xyz);
+    xyz2 = imageLinearTransform(tif, rgb2xyz);
+    deImg = scielab(xyz1, xyz2, wp, params);
+    de(ii) = mean(deImg(:));
+end
--- EI_Figure/l3LumLevels_Fig3.m
+++ EI_Figure/l3LumLevels_Fig3.m
@@ -11,13 +11,14 @@ ieInit;
 
 % Init luminance levels
 nLumLevels = 10;
-levels = round(logspace(log10(4), log10(80), nLumLevels));
+levels = round(logspace(log10(4), log10(50), nLumLevels));
 
 % Init training parameters
 cfa = [2 1; 3 4]; % Bayer pattern, 2 and 4 are both for green
 patch_sz = [5 5];
 pad_sz   = (patch_sz - 1) / 2;
 offset = [1 2];
+r_offset = [1 0]; % rotated offset
 
 % Init S-CIELab parameters
 d = displayCreate('LCD-Apple');
@@ -31,9 +32,58 @@ params.sampPerDeg = displayGet(d, 'dots per deg');
 % Init remote data toolbox parameters
 rd = RdtClient('scien');
 rd.crp('/L3/Farrell/D200/garden');
+s = rd.searchArtifacts('dsc_');
+
+%% Test if the alignment is correct
+rotatedImage = [];
+badImage = [];
+for ii = 1 : length(s)
+    l3t = l3TrainOLS();
+    l3t.l3c.p_max = 1e4;
+    l3t.l3c.patchSize = patch_sz;
+    l3t.l3c.cutPoints = {logspace(-3.5, -1.2, 20), []};
+    
+    % load jpg
+    remoteURL = s(ii).url(1:end-3);
+    localFile = [tempname '.jpg'];
+    rdtSave(localFile, [remoteURL 'jpg']);
+    jpg = im2double(imread(localFile));
+    sz = [size(jpg, 1) size(jpg, 2)];
+    delete(localFile);
+    
+    % load raw
+    localFile = [tempname '.pgm'];
+    rdtSave(localFile, [remoteURL 'pgm']);
+    I_raw = im2double(imread(localFile));
+    raw = rawAdjustSize(I_raw, sz, pad_sz, offset);
+    delete(localFile);
+    
+    % train
+    l3t.train(l3DataCamera({raw}, {jpg}, cfa));
+    
+    % check
+    kernels = l3t.kernels(1:numel(cfa):end);
+    meanK = mean(cell2mat(reshape(kernels, [1 1 length(kernels)])), 3);
+    if all(max(meanK) ~= meanK(14, :)) % assume patch size is [5 5]
+        raw = rawAdjustSize(I_raw, sz, pad_sz, r_offset);
+        l3t.train(l3DataCamera({raw}, {jpg}, cfa), true);
+        
+        kernels = l3t.kernels(1:numel(cfa):end);
+        meanK = mean(cell2mat(reshape(kernels, [1 1 length(kernels)])), 3);
+        if any(max(meanK) == meanK(14, :))
+            fprintf('Rotated Image %d: %s\n', ii, s(ii).artifactId);
+            rotatedImage = cat(1, rotatedImage, ii);
+        else
+            fprintf('Bad Image %d: %s\n', ii, s(ii).artifactId);
+            badImage = cat(1, badImage, ii);
+        end
+    end
+end
+
+if ~isempty(badImage), error('should fix badImage before move on'); end
 
 %% Training on odd images and test on even ones
-s = rd.searchArtifacts('dsc_');
+%  The rotated images are still of pain. Let's not use them at this point
 de = zeros(nLumLevels, floor(length(s)/2), 2);
 l3r = l3Render();
 
@@ -41,11 +91,13 @@ l3r = l3Render();
 for ii = 1 : nLumLevels
     % Set training parameters
     l3t = l3TrainOLS();
+    l3t.l3c.p_max = 1e4;
     l3t.l3c.patchSize = patch_sz;
     l3t.l3c.cutPoints = {logspace(-4, -1.2, levels(ii)), []};
-    for jj = 1 : length(s)
+    for jj = 1 : 2 : length(s)
+        if any(jj == rotatedImage), continue; end
         % load jpg image
-        remoteURL = s(1).url(1:end-3);
+        remoteURL = s(jj).url(1:end-3);
         localFile = [tempname '.jpg'];
         rdtSave(localFile, [remoteURL 'jpg']);
         jpg = im2double(imread(localFile));
@@ -56,6 +108,7 @@ for ii = 1 : nLumLevels
         localFile = [tempname '.pgm'];
         rdtSave(localFile, [remoteURL 'pgm']);
         raw = im2double(imread(localFile));
+        
         raw = rawAdjustSize(raw, sz, pad_sz, offset);
         delete(localFile);
         
@@ -68,10 +121,13 @@ for ii = 1 : nLumLevels
     
     % learn linear filters
     l3t.train();
+    l3t.l3c.clearData();
+    save(sprintf('l3t_%d.mat', levels(ii)), 'l3t', 'rotatedImage', 'cfa');
     
     % render and compute deltaE
     for jj = 2 : 2 : length(s)
         % load jpg image
+        if any(jj==rotatedImage), continue; end
         remoteURL = s(jj).url(1:end-3);
         localFile = [tempname '.jpg'];
         rdtSave(localFile, [remoteURL 'jpg']);
@@ -93,6 +149,11 @@ for ii = 1 : nLumLevels
         xyz2 = imageLinearTransform(jpg, rgb2xyz);
         deImg = scielab(xyz1, xyz2, wp, params);
         de(ii, jj/2, :) = quantile(deImg(:), [0.5 0.9]);
+        fprintf('de: %.2f, %.2f\n', de(ii, jj/2, 1), de(ii, jj/2, 2)); 
+        % output file
+        if jj == 2
+            imwrite(l3_RGB, sprintf('l3_RGB_%d.jpg', levels(ii)));
+        end
     end
 end
 
