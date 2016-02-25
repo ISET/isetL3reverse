05160a77acadaaac5ed8157323d8538858b89ef6
Haomiao Jiang <Killua@sr13-5d76588a90.stanford.edu>
2016-02-25 11:44:32 -0800

    update underwater script

--- s_underwater.m
+++ s_underwater.m
@@ -8,31 +8,56 @@ ieInit
 %% Initialize parameters
 cfa = [2 1; 3 4];
 patch_sz = [5 5];
+base = 'http://scarlet.stanford.edu/validation/SCIEN/CARIBBEAN/03/';
+rawDir = [base 'Data/Water/'];
+rgbDir = [base 'Reference/Water/'];
+outDir = '~/SimResults/L3/Underwater/';
+
+s = lsScarlet(rgbDir, '.tif');
+nTrain = 100; % use 100 images in training
+train_indx = randperm(length(s), nTrain);
 
 %% Training
-%  load training images
-raw = im2double(imread('~/Downloads/Img_7465.pgm'));
-raw = raw(2:end, :); % make sure raw size is a multiple of cfa size
-rgb = im2double(imread('~/Downloads/Img_7465_corr.png'));
-rgb = rgb(2:end, :, :);
-
+%  Initialize training object
 l3t = l3TrainRidge();
 l3t.l3c.patchSize = patch_sz;
-l3t.l3c.cutPoints = {logspace(-2.2, -1.1, 40), []};
-l3t.train(l3DataCamera({raw}, {rgb}, cfa));
+l3t.l3c.cutPoints = {logspace(-2.2, -1.5, 40), []};
+l3t.l3c.p_max = 1000;
+
+%  classify training samples
+for ii = train_indx
+    raw = im2double(imread([rawDir s(ii).name(1:end-4) '.pgm']));
+    raw = raw(2:end, :); % make sure raw size is a multiple of cfa size
+    rgb = im2double(imread([rgbDir s(ii).name]));
+    rgb = rgb(2:end, :, :);
+    
+    l3t.l3c.classify(l3DataCamera({raw}, {rgb}, cfa));
+end
+
+% learn kernels
+l3t.train();
 
 %% Rendering
-%  first, we render on the image we trained on
+%  Init render object
 l3r = l3Render();
-l3_RGB = ieClip(l3r.render(raw, cfa, l3t), 0 ,1);
-vcNewGraphWin; imshow(l3_RGB);
+de = zeros(length(s), 1);
 
-%  load test image
-raw_test = im2double(imread('~/Downloads/Img_4425.pgm'));
-raw_test = raw_test(2:end, :);
-rgb_test = im2double(imread('~/Downloads/Img_4425_corr.png'));
-rgb_test = rgb_test(2:end, :, :);
-
-l3_RGB = ieClip(l3r.render(raw_test, cfa, l3t), 0, 1);
-vcNewGraphWin; imshow(rgb_test); title('Target');
-vcNewGraphWin; imshow(l3_RGB); title('L3');
+for ii = 1 : length(s)
+    % load data
+    raw = im2double(imread([rawDir s(ii).name(1:end-4) '.pgm']));
+    raw = raw(2:end, :); % make sure raw size is a multiple of cfa size
+    rgb = im2double(imread([rgbDir s(ii).name]));
+    rgb = rgb(2:end, :, :);
+    
+    % render
+    l3_RGB = ieClip(l3r.render(raw, cfa, l3t), 0 ,1);
+    
+    % write rendered image out
+    imwrite(l3_RGB, [outDir s(ii).name(1:end-4) '.jpg']);
+    
+    % compute deltaE
+    xyz1 = RGB2XWFormat(srgb2xyz(l3_RGB));
+    xyz2 = RGB2XWFormat(srgb2xyz(rgb));
+    
+    de(ii) = mean(deltaEab(xyz1, xyz2, max(xyz2)));
+end
